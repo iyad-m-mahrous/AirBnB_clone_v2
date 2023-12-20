75e82bc9b2280b813c003b5c2d96ffaafa9e9dbe
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-20 12:25:37 -0800

    Task 7

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
-iimport models
+import models
 
 Base = declarative_base()
 
